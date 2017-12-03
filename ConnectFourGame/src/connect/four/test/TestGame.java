2175a83e89b0951b107c7916f2608c0de3a20c1c
leecordle89 <micronya89@gmail.com>
2017-12-03 09:02:04 -0700

    Some more tests
    Still some more to come for testGame

--- ConnectFourGame/src/connect/four/test/TestGame.java
+++ ConnectFourGame/src/connect/four/test/TestGame.java
@@ -1,24 +1,56 @@
 package connect.four.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.Before;
 
-public class TestGame {
+import connect.four.Game;
+import connect.four.board.Board;
+import connect.four.player.ConsolePlayer;
+import connect.four.player.Player;
 
+
+public class TestGame 
+{
+
+	ConsolePlayer playerOne;
+	ConsolePlayer playerTwo;
+	int inRow = 4;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp()
+	{
+		playerOne = new ConsolePlayer("X");
+		playerTwo = new ConsolePlayer("@");
 	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testDiagonalWinner() 
+	{
+		//DiagonalWin
+		Object[][] board1Layout = { { playerTwo, null, null, null, null, null }, { playerTwo, playerTwo, playerTwo, null, null, null },
+				{ playerOne, playerOne, playerTwo, null, null, null }, { playerTwo, playerOne, playerTwo, playerTwo, null, null} };
+		Board a = new Board(board1Layout.length, board1Layout[0].length);
+		for (int i = 0; i < a.getWidth(); i++) 
+			for (int j = 0; j < a.getHeight(); j++) 
+				a.play(i, (Player) board1Layout[i][j]);
 
+		Board board1 = new Board(a);
+
+		assertEquals(playerTwo, Game.detectWinner(board1, inRow));
+	}
+	
+	@Test
+	public void testHorizontalWinner()
+	{
+		
+	}
+	
+	@Test
+	public void testVerticalWinner()
+	{
+		
+	}
 }
